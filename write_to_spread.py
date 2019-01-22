756dfd2b72e31ab5311c972fb03daa497c2f97b3
kconnolly101 <Olimpik118@>
2019-01-22 17:21:13 +0000

    change column_dict values

--- write_to_spread.py
+++ write_to_spread.py
@@ -17,17 +17,17 @@ def spread(file_path, output):
                    'away_team_name': 3,
                    'home_goal_total': 4,
                    'away_goal_total': 5,
-                   'referee': 44,
-                   'home_corners': 45,
-                   'away_corners': 46,
-                   'home_shots_on': 51,
-                   'away_shots_on': 53,
-                   'home_shots_wide': 52,
-                   'away_shots_wide': 54,
-                   'home_fouls': 49,
-                   'away_fouls': 50,
-                   'home_offsides': 47,
-                   'away_offsides': 48,
+                   'referee': 46,
+                   'home_corners': 47,
+                   'away_corners': 48,
+                   'home_shots_on': 53,
+                   'away_shots_on': 55,
+                   'home_shots_wide': 54,
+                   'away_shots_wide': 56,
+                   'home_fouls': 51,
+                   'away_fouls': 52,
+                   'home_offsides': 49,
+                   'away_offsides': 50,
                    }
 
     # write minute related markets
@@ -50,7 +50,7 @@ def spread(file_path, output):
         away_goal_col += 1
         j += 1
 
-    home_yel_col = 20
+    home_yel_col = 22
     j = 0
 
     for i in output['home_yellow_times']:
@@ -58,7 +58,7 @@ def spread(file_path, output):
         home_yel_col += 1
         j += 1
 
-    away_yel_col = 29
+    away_yel_col = 31
     j = 0
 
     for i in output['away_yellow_times']:
@@ -66,7 +66,7 @@ def spread(file_path, output):
         away_yel_col += 1
         j += 1
 
-    home_red_col = 38
+    home_red_col = 41
     j = 0
 
     for i in output['home_red_times']:
@@ -74,7 +74,7 @@ def spread(file_path, output):
         home_red_col += 1
         j += 1
 
-    away_red_col = 41
+    away_red_col = 43
     j = 0
 
     for i in output['away_red_times']:
