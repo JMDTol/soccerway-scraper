a9bf4fe0d5ad08e9226394bba0409fd02898647e
kconnolly101 <Olimpik118@>
2019-01-23 17:00:47 +0000

    Speed up write function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from scraper import soccerway_scraper
 from tkinter import filedialog
 from write_to_spread import spread
+from openpyxl import load_workbook
 import tkinter as tk
+import timeit
+
 
 root = tk.Tk()
 root.withdraw()
@@ -10,8 +13,19 @@ file_path = filedialog.askopenfilename()
 url = input("Enter URL (split multiple URLs with ','): ")
 url_list = url.split(',')
 
+wb = load_workbook(file_path)
+ws = wb.worksheets[0]
+
+start = timeit.default_timer()
+
 for url in url_list:
     output = soccerway_scraper(url)
-    spread(file_path, output)
+    spread(output, ws)
 
+wb.save(file_path)
+
+stop = timeit.default_timer()
+
+print("=" * 70)
+print('Time: ', stop - start)
 print("=" * 70 + "\nComplete - {} matches added".format(len(url_list)))
