0d3ae24e045186aba4ec2d0bf0b2e2ce4d9c2249
kconnolly101 <Olimpik118@>
2019-01-25 10:25:52 +0000

    Change variable names

--- write_to_spread.py
+++ write_to_spread.py
@@ -12,46 +12,49 @@ def spread(output, file_path):
     for i in ws.rows:
         column_length += 1
 
-    # dict values correspond to columns in spreadsheet
-    column_dict = {'date': 1,
-                   'home_team_name': 2,
-                   'away_team_name': 3,
-                   'home_goal_total': 4,
-                   'away_goal_total': 5,
-                   'referee': 46,
-                   'home_corners': 47,
-                   'away_corners': 48,
-                   'home_shots_on': 53,
-                   'away_shots_on': 55,
-                   'home_shots_wide': 54,
-                   'away_shots_wide': 56,
-                   'home_fouls': 51,
-                   'away_fouls': 52,
-                   'home_offsides': 49,
-                   'away_offsides': 50,
-                   'home_pens': 57,
-                   'away_pens': 58,
-                   'home_pen_mins': 59,
-                   'away_pen_mins': 60
-                   }
+    # dictionary values correspond to columns in spreadsheet where data should be written to
+    main_data = {'date': 1,
+                 'home_team_name': 2,
+                 'away_team_name': 3,
+                 'home_goal_total': 4,
+                 'away_goal_total': 5,
+                 'referee': 46,
+                 'home_corners': 47,
+                 'away_corners': 48,
+                 'home_shots_on': 53,
+                 'away_shots_on': 55,
+                 'home_shots_wide': 54,
+                 'away_shots_wide': 56,
+                 'home_fouls': 51,
+                 'away_fouls': 52,
+                 'home_offsides': 49,
+                 'away_offsides': 50,
+                 'home_pens': 57,
+                 'away_pens': 58,
+                 'home_pen_mins': 59,
+                 'away_pen_mins': 60
+                 }
 
-    min_col = [('home_goal_times', 6),
-               ('away_goal_times', 14),
-               ('home_yellow_times', 22),
-               ('away_yellow_times', 31),
-               ('home_red_times', 40),
-               ('away_red_times', 43)]
+    minutes_data = [('home_goal_times', 6),
+                    ('away_goal_times', 14),
+                    ('home_yellow_times', 22),
+                    ('away_yellow_times', 31),
+                    ('home_red_times', 40),
+                    ('away_red_times', 43)
+                    ]
 
-    for key in column_dict.keys():
-        ws.cell(row=column_length, column=column_dict[key]).value = output[key]
+    # write column_dict markets to spreadsheet
+    for key in main_data.keys():
+        ws.cell(row=column_length, column=main_data[key]).value = output[key]
 
-    for i in range(0, len(min_col)):
-        string = min_col[i][0]
+    # write min_col markets to spreadsheet
+    for i in range(0, len(minutes_data)):
+        string = minutes_data[i][0]
         length = len(output[string])
-        start = min_col[i][1]
+        start_col = minutes_data[i][1]
         for j in range(0, length):
-            ws.cell(row=column_length, column=start).value = output[string][j]
-            start += 1
+            ws.cell(row=column_length, column=start_col).value = output[string][j]
+            start_col += 1
 
     wb.save(file_path)
 
