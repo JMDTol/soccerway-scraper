7b4ea9699ea5bf26c49211a2d1e2af1f32165b02
kconnolly101 <Olimpik118@>
2019-01-27 13:04:20 +0000

    Remove Selenium function

--- get_urls.py
+++ get_urls.py
@@ -1,56 +1,76 @@
-import time
+from tkinter import filedialog
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from openpyxl import load_workbook
 from bs4 import BeautifulSoup
+import time
+import tkinter as tk
 
 
-def get_entire_season(url):
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
 
-    urls = []
+url = input('Input season URL: ')
 
-    chrome_options = Options()
-    chrome_options.add_argument("--kiosk")
+driver = webdriver.Chrome()
+driver.fullscreen_window()
+driver.get(url)
 
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get(url)
+time.sleep(1)
 
-    time.sleep(1)
+if driver.find_element_by_class_name('qc-cmp-button'):
+    privacy = driver.find_element_by_class_name('qc-cmp-button')
+    privacy.click()
 
-    if driver.find_element_by_class_name('qc-cmp-button'):
-        privacy = driver.find_element_by_class_name('qc-cmp-button')
-        privacy.click()
+time.sleep(1)
 
-    time.sleep(1)
+week = driver.find_element_by_id('page_competition_1_block_competition_matches_summary_5_1_2')
+week.click()
 
-    week = driver.find_element_by_id('page_competition_1_block_competition_matches_summary_5_1_2')
-    week.click()
+time.sleep(1)
 
-    time.sleep(2)
+html = driver.find_element_by_tag_name('html').get_attribute('innerHTML')
+game_week_soup = BeautifulSoup(html, 'html.parser')
 
+previous_clicks = 0
+url_list = []
+
+for week in game_week_soup.findAll(id='page_competition_1_block_competition_matches_summary_5_page_dropdown'):
+    number_weeks = (week.contents[-1])
+    previous_clicks = int(number_weeks.contents[0]) - 1
+
+for info in game_week_soup.findAll('td', class_='info-button button'):
+    for link in info.find_all('a', href=True):
+        match_url = 'https://us.soccerway.com' + link.get('href')
+        url_list.append(match_url)
+
+for i in range(previous_clicks):
+    previous_button = driver.find_element_by_class_name('previous')
+    previous_button.click()
+    time.sleep(0.1)
     html = driver.find_element_by_tag_name('html').get_attribute('innerHTML')
     game_week_soup = BeautifulSoup(html, 'html.parser')
-
-    previous_clicks = 0
-
-    for week in game_week_soup.findAll(id='page_competition_1_block_competition_matches_summary_5_page_dropdown'):
-        number_weeks = (week.contents[-1])
-        previous_clicks = int(number_weeks.contents[0]) - 1
-
     for info in game_week_soup.findAll('td', class_='info-button button'):
         for link in info.find_all('a', href=True):
             match_url = 'https://us.soccerway.com' + link.get('href')
-            urls.append(match_url)
+            url_list.append(match_url)
 
-    for i in range(previous_clicks):
-        previous_button = driver.find_element_by_class_name('previous')
-        previous_button.click()
-        time.sleep(1)
-        html = driver.find_element_by_tag_name('html').get_attribute('innerHTML')
-        game_week_soup = BeautifulSoup(html, 'html.parser')
-        for info in game_week_soup.findAll('td', class_='info-button button'):
-            for link in info.find_all('a', href=True):
-                match_url = 'https://us.soccerway.com' + link.get('href')
-                urls.append(match_url)
+driver.quit()
 
-    print('{} matches found\n'.format(len(urls)) + '=' * 70)
-    return urls
+print('=' * 70 + '\n{} matches added to {}'.format(len(url_list), file_path))
+
+wb = load_workbook(file_path)
+ws = wb.active
+
+column_length = 0
+
+for i in ws.rows:
+    column_length += 1
+
+url_list_reversed = url_list[::-1]
+
+for url in url_list_reversed:
+    ws.cell(row=column_length, column=1).value = url
+    column_length += 1
+
+wb.save(file_path)
