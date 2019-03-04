3b47384b6e3a42c0d7af6adea0b31c3a3374aef7
Kerwin <kerwinconnolly@gmail.com>
2019-03-04 19:05:36 +0000

    Create class

--- main.py
+++ main.py
@@ -6,34 +6,36 @@ import tkinter as tk
 import time
 
 
-# Open a dialog box to choose a spreadsheet for the data to be written to.
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
+class Main:
+    def __init__(self, spreadsheet_path):
+        self.path = spreadsheet_path
+        self.url_list = []
+        self.pause = 0
 
-check_season = input('Scrape entire season?: ')
+    def scraper(self):
+        if input('Scrape entire season?: ') == 'y':
+            self.url_list = get_urls(input('Enter season URL: '))
+            check_url_number = input('Continue?: ')
+            if check_url_number != 'y':
+                exit()
+        else:
+            self.url_list = input("Enter match URLs (split multiple URLs with ','): ").split(',')
 
-if check_season == 'y':
-    url_list = get_urls(input('Enter season URL: '))
-    check_url_number = input('Continue?: ')
-    if check_url_number != 'y':
-        exit()
-else:
-    url_list = input("Enter match URLs (split multiple URLs with ','): ").split(',')
+        for url in self.url_list:
+            if self.pause == 10:
+                time.sleep(10)
+                spread(scrape_match(url), self.path)
+                self.pause = 0
+            else:
+                time.sleep(1)
+                spread(scrape_match(url), self.path)
+                self.pause += 1
 
-print("=" * 100)
+        print("=" * 100 + "\nComplete - {} matches added".format(len(self.url_list)))
 
-# Pause loop every ten iterations to prevent requests being denied.
-pause = 0
 
-for url in url_list:
-    if pause == 10:
-        time.sleep(10)
-        spread(scrape_match(url), file_path)
-        pause = 0
-    else:
-        time.sleep(1)
-        spread(scrape_match(url), file_path)
-        pause += 1
-
-print("=" * 100 + "\nComplete - {} matches added".format(len(url_list)))
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+    main = Main(filedialog.askopenfilename())
+    main.scraper()
