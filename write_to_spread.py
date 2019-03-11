f6afb89c8806901434ade445711f5e7c15490456
kconnolly101 <kerwinconnolly@googlemail.com>
2019-03-11 16:15:54 +0000

    Rename variables

--- write_to_spread.py
+++ write_to_spread.py
@@ -6,8 +6,8 @@ def spread(match_dict, file_path):
     ws = wb.worksheets[0]
     write_row = ws.max_row + 1
 
-    # integers in main_data and time_data correspond to spreadsheet column numbers.
-    main_data = {'week': 1,
+    # integers in main_cols and time_cols correspond to spreadsheet column numbers.
+    main_cols = {'week': 1,
                  'date': 2,
                  'home_team_name': 3,
                  'away_team_name': 4,
@@ -30,7 +30,7 @@ def spread(match_dict, file_path):
                  'away_pen_mins': 61,
                  }
 
-    time_data = {'home_goal_times': 7,
+    time_cols = {'home_goal_times': 7,
                  'away_goal_times': 15,
                  'home_yellow_times': 23,
                  'away_yellow_times': 32,
@@ -38,16 +38,16 @@ def spread(match_dict, file_path):
                  'away_red_times': 44
                  }
 
-    # Write main_data markets to sheet.
-    for key in main_data.keys():
-        ws.cell(row=write_row, column=main_data[key]).value = match_dict[key]
+    # Write main_cols markets to sheet.
+    for stat in main_cols:
+        ws.cell(row=write_row, column=main_cols[stat]).value = match_dict[stat]
 
-    # Write time_data markets to sheet.
-    for key in time_data:
-        number_items = len(match_dict[key])
-        start_col = time_data[key]
+    # Write time_cols markets to sheet.
+    for stat in time_cols:
+        number_items = len(match_dict[stat])
+        start_col = time_cols[stat]
         for item in range(0, number_items):
-            ws.cell(row=write_row, column=start_col).value = match_dict[key][item]
+            ws.cell(row=write_row, column=start_col).value = match_dict[stat][item]
             start_col += 1
 
     wb.save(file_path)
