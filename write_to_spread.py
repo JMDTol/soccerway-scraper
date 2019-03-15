f40f609964976aa06369cd955c183aeaaec9146a
Kerwin <kerwinconnolly@googlemail.com>
2019-03-15 15:35:52 +0000

    Rename row variable

--- write_to_spread.py
+++ write_to_spread.py
@@ -4,7 +4,7 @@ from openpyxl import load_workbook
 def spread(match_dict, file_path):
     wb = load_workbook(file_path)
     ws = wb.worksheets[0]
-    write_row = ws.max_row + 1
+    row = ws.max_row + 1
 
     # integers in main_cols and time_cols correspond to spreadsheet column numbers.
     main_cols = {'week': 1,
@@ -36,13 +36,13 @@ def spread(match_dict, file_path):
 
     # Write main_cols markets to sheet.
     for stat, col in main_cols.items():
-        ws.cell(row=write_row, column=col).value = match_dict[stat]
+        ws.cell(row=row, column=col).value = match_dict[stat]
 
     # Write time_cols markets to sheet.
     for stat, start_col in time_cols.items():
         number_items = len(match_dict[stat])
         for item in range(number_items):
-            ws.cell(row=write_row, column=start_col).value = match_dict[stat][item]
+            ws.cell(row=row, column=start_col).value = match_dict[stat][item]
             start_col += 1
 
     wb.save(file_path)
