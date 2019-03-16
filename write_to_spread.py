6602545e0dcccae93a98346a8069682e18403aff
kconnolly101 <kerwinconnolly@gmail.com>
2019-03-16 17:24:49 +0000

    Format dict

--- write_to_spread.py
+++ write_to_spread.py
@@ -7,32 +7,34 @@ def spread(match_dict, file_path):
     row = ws.max_row + 1
 
     # integers in main_cols and time_cols correspond to spreadsheet column numbers.
-    main_cols = {'week': 1,
-                 'date': 2,
-                 'home_team_name': 3,
-                 'away_team_name': 4,
-                 'home_goal_total': 5,
-                 'away_goal_total': 6,
-                 'referee': 47,
-                 'home_corners': 48,
-                 'away_corners': 49,
-                 'home_shots_on': 54,
-                 'away_shots_on': 56,
-                 'home_shots_wide': 55,
-                 'away_shots_wide': 57,
-                 'home_fouls': 52,
-                 'away_fouls': 53,
-                 'home_offsides': 50,
-                 'away_offsides': 51,
-                 }
+    main_cols = {
+        'week': 1,
+        'date': 2,
+        'home_team_name': 3,
+        'away_team_name': 4,
+        'home_goal_total': 5,
+        'away_goal_total': 6,
+        'referee': 47,
+        'home_corners': 48,
+        'away_corners': 49,
+        'home_shots_on': 54,
+        'away_shots_on': 56,
+        'home_shots_wide': 55,
+        'away_shots_wide': 57,
+        'home_fouls': 52,
+        'away_fouls': 53,
+        'home_offsides': 50,
+        'away_offsides': 51,
+    }
 
-    time_cols = {'home_goal_times': 7,
-                 'away_goal_times': 15,
-                 'home_yellow_times': 23,
-                 'away_yellow_times': 32,
-                 'home_red_times': 41,
-                 'away_red_times': 44
-                 }
+    time_cols = {
+        'home_goal_times': 7,
+        'away_goal_times': 15,
+        'home_yellow_times': 23,
+        'away_yellow_times': 32,
+        'home_red_times': 41,
+        'away_red_times': 44
+    }
 
     # Write main_cols markets to sheet.
     for stat, col in main_cols.items():
