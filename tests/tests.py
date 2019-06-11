4653d8dbc747e42ad5aaa7606cf206f13de421f6
kconnolly101 <kerwinconnolly@gmail.com>
2019-06-11 15:13:12 +0100

    Format

--- tests/tests.py
+++ tests/tests.py
@@ -8,10 +8,11 @@ import scraper
 
 
 class TestScraper(unittest.TestCase):
-
     def setUp(self):
-        response = requests.get('https://us.soccerway.com/matches/2019/04/12/australia/a-league/brisbane-roar-fc/wellington-phoenix/2835039/?ICID=PL_MS_06')
-        self.soup = BeautifulSoup(response.text, 'html.parser')
+        response = requests.get(
+            "https://us.soccerway.com/matches/2019/04/12/australia/a-league/brisbane-roar-fc/wellington-phoenix/2835039/?ICID=PL_MS_06"
+        )
+        self.soup = BeautifulSoup(response.text, "html.parser")
 
     def test_week(self):
         self.assertEqual(scraper.game_week(self.soup), 25)
@@ -20,11 +21,12 @@ class TestScraper(unittest.TestCase):
         self.assertEqual(scraper.date(self.soup), datetime.date(2019, 4, 12))
 
     def test_team_names(self):
-        self.assertEqual(scraper.team_names(self.soup),
-                         ('Brisbane Roar', 'Wellington Phoenix'))
+        self.assertEqual(
+            scraper.team_names(self.soup), ("Brisbane Roar", "Wellington Phoenix")
+        )
 
     def test_referee(self):
-        self.assertEqual(scraper.referee(self.soup), 'P. Green')
+        self.assertEqual(scraper.referee(self.soup), "P. Green")
 
     def test_home_goals(self):
         self.assertEqual(scraper.home_goals(self.soup), [37, 42])
@@ -33,23 +35,22 @@ class TestScraper(unittest.TestCase):
         self.assertEqual(scraper.away_goals(self.soup), [53])
 
     def test_home_cards(self):
-        self.assertEqual(scraper.home_cards(self.soup),
-                         ([32, 34, 83, 90], [90]))
+        self.assertEqual(scraper.home_cards(self.soup), ([32, 34, 83, 90], [90]))
 
     def test_away_cards(self):
         self.assertEqual(scraper.away_cards(self.soup), ([63], []))
 
     def test_stats(self):
         stats = {
-            'away_corners': 6,
-            'away_fouls': 18,
-            'away_offsides': 0,
-            'away_shots_on': 4,
-            'away_shots_wide': 8,
-            'home_corners': 6,
-            'home_fouls': 19,
-            'home_offsides': 2,
-            'home_shots_on': 6,
-            'home_shots_wide': 11
+            "away_corners": 6,
+            "away_fouls": 18,
+            "away_offsides": 0,
+            "away_shots_on": 4,
+            "away_shots_wide": 8,
+            "home_corners": 6,
+            "home_fouls": 19,
+            "home_offsides": 2,
+            "home_shots_on": 6,
+            "home_shots_wide": 11,
         }
         self.assertEqual(scraper.scrape_iframe(self.soup), stats)
